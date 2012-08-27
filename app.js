a7c3a09a4d5bf52b54b12d727d63120b68c9ae6c
Jamie Knight <jamie@jkg3.com>
2012-08-27 16:28:57 +0100

    Basic HTML5 / JS BBC Radio playback console.

---
+++ app.js
@@ -0,0 +1,45 @@
+var radioApp = (function(){
+	var stations, eventType, station,
+	playingStation = [];
+	
+	var hasTouchSupport = function(){
+	     try {  
+	        document.createEvent("TouchEvent");
+	        return true;
+	    } catch (e) {
+	        return false;   
+	    }
+	}
+	
+	var togglePlayback = function(e){
+		e.preventDefault();
+		var station = (e.target);
+			id = station.id,
+			url = station.href;
+		
+		if(playingStation[id]){
+			playingStation[id].pause();
+			delete playingStation[id];
+		}else{
+			playingStation[id] = new Audio(url);
+			playingStation[id].play();
+		}
+	}
+
+	var init = function(){
+		stations = document.getElementsByClassName('station');
+		eventType = (radioApp.hasTouchSupport())? 'ontouchstart' : 'click' ;
+		for (i=0; i<stations.length; i++)
+		{
+			stations[i].addEventListener(eventType, radioApp.togglePlayback, false);
+		}
+	}
+  
+	return {
+		init: init,
+		hasTouchSupport: hasTouchSupport,
+		togglePlayback: togglePlayback
+	}
+})();
+
+radioApp.init();
