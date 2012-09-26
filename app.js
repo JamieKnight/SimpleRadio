e5eabb648775700a60eb9c3e92ee6f38220b6c6d
jamie knight <jamie.knight@bbc.co.uk>
2012-09-26 10:48:45 +0100

    Tidy Up JS
    Tidy up the JS to be self executing closure.

--- app.js
+++ app.js
@@ -1,60 +1,53 @@
-var radioApp = (function(){
-	var stations, eventType, station,
-	playingStations = [];
-	
-	var hasTouchSupport = function(){
-	     try {  
-	        document.createEvent("TouchEvent");
-	        return true;
-	    } catch (e) {
-	        return false;   
-	    }
-	}
-	
-	var toggleClass = function(targetClass){
-		console.log('class');
-	}
-	
-	var togglePlayback = function(e){
-		e.preventDefault();
-		var station = (e.target);
-			id = station.id,
-			url = station.href,
-			label = station.getElementsByClassName('sta-play-btn');
-		
-		station.parentNode.classList.toggle('sta-playing');
-		
-		
-		
-		
-		
-		if(playingStations[id]){
-			playingStations[id].pause();
-			delete playingStations[id];
-			label[0].innerHTML = "Play";
-		}else{
-			playingStations[id] = new Audio(url);
-			playingStations[id].play();
-			label[0].innerHTML = "Pause";
-		}
-		
-		return false;
-	}
+(function(){
+    var stations, eventType, station, playingStations;
+    
+    playingStations = new Array;
 
-	var init = function(){
-		stations = document.getElementsByClassName('station');
-		eventType = (radioApp.hasTouchSupport())? 'touchstart' : 'click' ;
-		for (i=0; i<stations.length; i++)
-		{
-			stations[i].addEventListener(eventType, radioApp.togglePlayback, false);
-		}
-	}
-  
-	return {
-		init: init,
-		hasTouchSupport: hasTouchSupport,
-		togglePlayback: togglePlayback,
-	}
+    var hasTouchSupport = function(){
+        try {  
+            document.createEvent("TouchEvent");
+            return true;
+        } catch (e) {
+            return false;
+        }
+    }
+
+    var togglePlayback = function(e){
+        e.preventDefault();
+        var station = (e.target);
+            id = station.id,
+            url = station.href,
+            label = station.getElementsByClassName('sta-play-btn');
+            station.parentNode.classList.toggle('sta-playing');
+
+            console.log(playingStations);
+
+        if(playingStations[id]){
+            playingStations[id].pause();
+            delete playingStations[id];
+            label[0].innerHTML = "Play";
+        }else{
+            playingStations[id] = new Audio(url);
+            playingStations[id].play();
+            label[0].innerHTML = "Pause";
+        }
+        return false;
+    }
+    
+    var bindStationPlayback = function(){
+        stations = document.getElementsByClassName('station');
+        eventType = (hasTouchSupport())? 'touchstart' : 'click' ;
+        for (i=0; i<stations.length; i++)
+        {
+            stations[i].addEventListener(eventType, togglePlayback, false);
+        }
+    }
+
+    var init = function(){
+        bindStationPlayback();
+        
+    }
+
+    //kick everything off
+    init();
 })();
-
-radioApp.init();
